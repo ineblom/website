9ff029a729d59220023ddf1dc663182b9dd7b516
Theodor Ineblom <theodor.ineblom@gmail.com>
2025-03-09 00:31:30 +0100

    Include projects html pages in dist bulid

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   root: "src",
   publicDir: "../public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        phonebook: 'src/projects/phonebook.html',
+        goldenGrouse: 'src/projects/golden-grouse.html',
+      },
+    },
   },
   plugins: [
     tailwindcss(),
