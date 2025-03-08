2218737b9f9f89ebd781dbeb3abeb4c30a7307b8
Theodor Ineblom <theodor.ineblom@gmail.com>
2025-03-09 00:09:50 +0100

    Stop dist from appearing in src

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   root: "src",
   publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   plugins: [
     tailwindcss(),
   ],
